080367a02bcc6318f4c55c947654e6a1a5ca1621
SrinuVadlapudi <87997023+SrinuVadlapudi@users.noreply.github.com>
2021-08-12 22:34:46 +0530

    Update main.tf

--- VM-VPC/main.tf
+++ VM-VPC/main.tf
@@ -1,15 +1,15 @@
 provider "google" {
   project = "srinu2"
   credentials = file("srinu2-14f292a3a4e3.json")
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
+  region  = "asia-east2-b"
+  zone    = "asia-east2-b"
 }
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network-8"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "terraform-instance-ink"
   machine_type = "f1-micro"
 
   boot_disk {
